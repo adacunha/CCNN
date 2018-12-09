f8ca55f5c9099e389745a5a5ae3c41d5f458b369
Antonio da Cunha <antonio.dacunha@wustl.edu>
2018-12-09 02:48:10 -0600

    working?? prob not

---
+++ Checkers/src/Parse_Game_Data.py
@@ -0,0 +1,84 @@
+import Checkers
+import re
+
+raw_data_path = "../data/games.txt"
+parsed_data_path = "../data/parsed_games.txt"
+
+board = Checkers.Board()
+
+def get_turn_coords(turn):
+	squares = turn.split('-')
+	if len(squares) == 1:
+		squares = turn.split('x')
+	if len(squares) < 2:
+		print("TURN PARSING FUCKED UP ON: " + turn)
+	from_square = int(squares[0])
+	to_square = int(squares[len(squares)-1])
+	return (from_square, to_square)
+
+def get_turn_data(board, current_player, turn):
+	turn_coords = get_turn_coords(turn)
+	return (board.get_nn_input(current_player), turn_coords)	
+
+def make_move(board, turn):
+	jumps = turn.split('x')
+	if len(jumps) == 1:
+		board.move_piece(get_turn_coords(turn))
+		return
+	for i in range(1, len(jumps)):
+		last = int(jumps[i-1])
+		next = int(jumps[i])	
+		board.capture((last, next))
+
+def get_game_data(board, game_str):
+
+	turns = game_str.split()
+
+	black_piece = board.get_black_piece()
+	white_piece = board.get_white_piece()
+	current_player = black_piece
+
+	turn_data = []
+
+	for i, turn in enumerate(turns[:-1]):
+		if not i%3:
+			continue
+		move_data = get_turn_data(board, current_player, turn)
+		turn_data.append(move_data)
+		make_move(board, turn)
+		current_player = black_piece if current_player == white_piece else white_piece		
+
+	return turn_data
+
+train_data = []
+
+with open(raw_data_path, 'r') as data_file:
+	game_str = ""
+	for line in data_file:
+		if line[0] != '[':
+			line = line.strip()
+			if len(line) == 0:
+				orig_game_str = game_str
+				game_str = re.sub(r'\{[^\}]*\}', '', game_str)
+				try:
+					train_data.extend(get_game_data(board, game_str))
+				except:
+					print()
+					print("EXCEPTION ON: ")
+					print(orig_game_str)
+					print(game_str)
+					print()
+				board.refresh()
+				game_str = ""
+			else:
+				game_str = game_str + " " + line
+
+with open(parsed_data_path, 'w') as output_file:
+	for data_point in train_data:
+		x = data_point[0]
+		y = data_point[1]	
+		for val in x:
+			output_file.write(str(val) + " ")
+		for val in y:
+			output_file.write(str(val) + " ")
+		output_file.write("\n")
--- Checkers/src/train_policy.py
+++ Checkers/src/train_policy.py
@@ -1,69 +1,43 @@
-import Checkers
 import tensorflow as tf
+import os
+from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
+model_name = "mark1_64r"
 
-data_path = "../data/games.txt"
+data = []
+labels = []
 
-board = Checkers.Board()
+train_data_file = "../data/parsed_games.txt"
+with open(train_data_file) as dat_file:
+	for line in dat_file:
+		raw_data = line.strip().split()	
+		x = [int(x) for x in raw_data[0:32]]
+		y = [int(y) for y in raw_data[32:34]]
+		data.append(x)
+		labels.append(y)
 
-def get_turn_coords(turn):
-	squares = turn.split('-')
-	if len(squares) == 1:
-		squares = turn.split('x')
-	if len(squares) < 2:
-		print("TURN PARSING FUCKED UP ON: " + turn)
-	from_square = int(squares[0])
-	to_square = int(squares[len(squares)-1])
-	return (from_square, to_square)
+data = np.array(data)
+labels = np.array(labels)
 
-def get_turn_data(board, current_player, turn):
-	turn_coords = get_turn_coords(turn)
-	return (board.get_nn_input(current_player), turn_coords)	
+print(data)
+print(labels)
 
-def make_move(board, turn):
-	jumps = turn.split('x')
-	if len(jumps) == 1:
-		board.move_piece(get_turn_coords(turn))
-		return
-	for i in range(1, len(jumps)):
-		last = int(jumps[i-1])
-		next = int(jumps[i])	
-		board.capture((last, next))
+model = tf.keras.models.Sequential([
+	keras.layers.Dense(64, activation=tf.nn.relu),
+    	keras.layers.Dense(2, activation=tf.nn.softmax)
+])
 
-def get_game_data(board, game_str):
+model.compile(optimizer='adam',
+              loss = 'categorical_crossentropy',
+              metrics=['accuracy'])
 
-	turns = game_str.split()
+checkpoint_path = "policy_network_checkpoints/" + model_name + ".ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
 
-	black_piece = board.get_black_piece()
-	white_piece = board.get_white_piece()
-	current_player = black_piece
+# Create checkpoint callback
+# Create checkpoint callback
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
 
-	turn_data = []
-
-	for i, turn in enumerate(turns[:-1]):
-		if not i%3:
-			continue
-		move_data = get_turn_data(board, current_player, turn)
-		turn_data.append(move_data)
-		make_move(board, turn)
-		current_player = black_piece if current_player == white_piece else white_piece		
-
-	return turn_data
-
-train_data = []
-
-with open(data_path, 'r') as data_file:
-	game_str = ""
-	for line in data_file:
-		if line[0] != '[':
-			line = line.strip()
-			if len(line) == 0:
-				train_data.extend(get_game_data(board, game_str))
-				board.refresh()
-				game_str = ""
-				break
-			else:
-				game_str = game_str + " " + line
-
-print(train_data)
+model.fit(data, labels, epochs=1, callbacks=[cp_callback])
