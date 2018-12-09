d4719ff14bf5d0e7be1814f9e4592038d980dd2e
Antonio da Cunha <antonio.dacunha@wustl.edu>
2018-12-09 15:23:49 -0600

    training networks

--- Checkers/src/Test_Policy.py
+++ Checkers/src/Test_Policy.py
@@ -1,17 +1,20 @@
 import numpy as np
 import os
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow import keras
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 import Checkers
 
-test_model_name = "100r"
+test_model_name = "64c_32c_1000r"
 
 model = tf.keras.models.Sequential([
-	layers.Dense(100, activation=tf.nn.relu),
-    	layers.Dense(128, activation=tf.nn.softmax)
+	keras.layers.Conv2D(64, kernel_size=(2, 2), strides=(1, 1), activation='relu', input_shape=(8,4,1)),
+	keras.layers.Conv2D(32, kernel_size=(2, 2), strides=(1, 1), activation='relu'),
+	keras.layers.Flatten(),
+    	keras.layers.Dense(1024, activation='relu'),
+    	keras.layers.Dense(128, activation=tf.nn.softmax)
 ])
 
 checkpoint_path = "policy_network_checkpoints/" + test_model_name + ".ckpt"
@@ -26,7 +29,14 @@ player_2 = board.get_white_piece()
 
 nn_input = board.get_nn_input(player_1)
 
-nn_first_move = model.predict(np.array([nn_input]))[0]
-first_move = np.argmax(predictions[0])
-print(nn_first_move)
-print(first_move)
+board_input = np.array([nn_input]).reshape((8, 4, 1));
+
+network_input = []
+network_input.append(board_input)
+network_input = np.array(network_input)
+
+predictions = model.predict(network_input)
+
+first_move_guess = np.argmax(predictions[0])
+
+print(first_move_guess)
